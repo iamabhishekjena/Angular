ae7596a77baf42fba9fa7dee62981ab0c667b8c2
Abhishek Jena <abhishek.jena@comviva.com>
2020-08-13 18:20:47 +0530

    routing commit

--- comps/src/app/app.module.ts
+++ comps/src/app/app.module.ts
@@ -2,18 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ElementsModule } from './elements/elements.module'; 
-import { CollectionsModule } from './collections/collections.module';
+import { AppComponent } from './app.component'; 
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    ElementsModule,
-    CollectionsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
